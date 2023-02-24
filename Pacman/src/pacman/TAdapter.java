f96b89ed620e96146e03c4268499ea746af39c7b
gioia.ascari <gioia.ascari96@gmail.com>
2023-02-24 17:51:40 +0100

    add metod for key pressed during the game and an option that if you click "escape" the game will be ended

--- Pacman/src/pacman/TAdapter.java
+++ Pacman/src/pacman/TAdapter.java
@@ -1,5 +1,45 @@
 package pacman;
 
-public class TAdapter {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class TAdapter extends KeyAdapter {
+	
+	private boolean inGame = false;
+	private boolean dying = false;
+	
+	private int pacmanX, pacmanY, pacmanDX, pacmanDY;
+	private int reqDX, reqDY;
+	
+	public void keyPressed(KeyEvent e) {
+		
+		int key = e.getKeyCode();
+		
+		if(inGame) {
+			
+			switch (key) {
+			case KeyEvent.VK_LEFT:
+				reqDX = -1;
+				reqDY = 0;
+				break;
+			case KeyEvent.VK_RIGHT:
+				reqDX = 1;
+				reqDY = 0;
+				break;
+			case KeyEvent.VK_UP:
+				reqDX = 0;
+				reqDY = 1;
+				break;
+			case KeyEvent.VK_DOWN:
+				reqDX = 0;
+				reqDY = -1;
+				break;
+			}
+			
+		}
+		
+	
+		
+	}
 
 }
